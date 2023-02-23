87fcaaf21243bf1b8d3233c0561ca18b2bf9304d
kittiescaties <122416863+kittiescaties@users.noreply.github.com>
2023-02-22 20:05:07 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+const https = require('https');
+const fs = require('fs');
+
+const invites = ['darkfollows']; // Add more invites here if needed
+
+fs.readFile('tokens.txt', 'utf8', (err, data) => {
+  if (err) throw err;
+
+  const tokens = data.split('\n').map(token => token.trim()).filter(token => token !== '');
+  console.log(`Found ${tokens.length} authorization tokens`);
+
+  for (const token of tokens) {
+    console.log(`Using authorization token ${token}`);
+
+    for (const invite of invites) {
+      const postData = JSON.stringify({
+        code: invite
+      });
+      const data = JSON.stringify({
+
+      });
+      const options = {
+        hostname: 'discord.com',
+        path: '/api/v10/invites/' + invite,
+        method: 'POST',
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0',
+            'Accept': '*/*',
+            'Accept-Language': 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Content-Type': 'application/json',
+            'X-Context-Properties': 'eyJsb2NhdGlvbiI6IkpvaW4gR3VpbGQiLCJsb2NhdGlvbl9ndWlsZF9pZCI6Ijk4OTkxOTY0NTY4MTE4ODk1NCIsImxvY2F0aW9uX2NoYW5uZWxfaWQiOiI5OTAzMTc0ODgxNzg4NjgyMjQiLCJsb2NhdGlvbl9jaGFubmVsX3R5cGUiOjB9',
+            'Authorization': token,
+            'X-Super-Properties': 'eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRmlyZWZveCIsImRldmljZSI6IiIsInN5c3RlbV9sb2NhbGUiOiJmciIsImJyb3dzZXJfdXNlcl9hZ2VudCI6Ik1vemlsbGEvNS4wIChXaW5kb3dzIE5UIDEwLjA7IFdpbjY0OyB4NjQ7IHJ2OjEwMi4wKSBHZWNrby8yMDEwMDEwMSBGaXJlZm94LzEwMi4wIiwiYnJvd3Nlcl92ZXJzaW9uIjoiMTAyLjAiLCJvc192ZXJzaW9uIjoiMTAiLCJyZWZlcnJlciI6IiIsInJlZmVycmluZ19kb21haW4iOiIiLCJyZWZlcnJlcl9jdXJyZW50IjoiIiwicmVmZXJyaW5nX2RvbWFpbl9jdXJyZW50IjoiIiwicmVsZWFzZV9jaGFubmVsIjoic3RhYmxlIiwiY2xpZW50X2J1aWxkX251bWJlciI6MTM2MjQwLCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsfQ==',
+            'X-Discord-Locale': 'en-US',
+            'X-Debug-Options': 'bugReporterEnabled',
+            'Origin': 'https://discord.com',
+            'DNT': '1',
+            'Connection': 'keep-alive',
+            'Referer': 'https://discord.com',
+            'Cookie': '__dcfduid=21183630021f11edb7e89582009dfd5e; __sdcfduid=21183631021f11edb7e89582009dfd5ee4936758ec8c8a248427f80a1732a58e4e71502891b76ca0584dc6fafa653638; locale=en-US',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+            'TE': 'trailers',
+
+        }
+      };
+
+      const req = https.request(options, (res) => {
+        let responseData = '';
+        res.on('data', (chunk) => {
+          responseData += chunk;
+        });
+        res.on('end', () => {
+          console.log(responseData);
+        });
+      });
+
+      req.on('error', (error) => {
+        console.error(error);
+      });
+
+      req.write(postData);
+      req.end();
+    }
+  }
+});
